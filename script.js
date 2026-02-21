4ff79ed7701d1b22bafb756a8fd50c158dff8622
Murugesh Padmanaban <muruugesh@Murugeshs-MacBook-Air.local>
2026-02-21 15:34:26 +0530

    html and css file are added

---
+++ script.js
@@ -0,0 +1,92 @@
+/*
+Primitive Data type
+String - "Hello world",'Hello world'
+Number - 1, 10.5, 10.44444
+Bollean - true or false
+undefined
+null
+
+------------
+
+Non Primitive Datatype
+
+Object - {
+name:"sam"
+dob:"10/12/35"
+}
+Array - [1,3,5,false, 'name',undefined, null, {}, []]
+
+function greet(){
+return 'Hello'
+}
+greet()
+
+
+-------------
+let city = "chennai"; //1111
+let city2 = city; //12232
+city = "karnataka";
+console.log(city);
+console.log(city2);
+
+let student = { name: "sam", age: 20 }; //123
+let student2 = student; //123
+student.name = "george";
+
+console.log(student.name, "student1");
+console.log(student2.name, "student2");
+
+var,let,const
+
+var
+*/
+
+// var name = "sam";
+// console.log(name);
+// name = 10;
+// name = "kumar";
+// console.log(name);
+
+// // console.log(name);
+// let city = "chennai";
+// city = "banglore";
+// console.log(city);
+
+// const sex = "M";
+// sex = 10;
+// console.log(sex);
+
+//Hoisting only available with var keyword
+//
+// let j = 11;
+// //function scope
+// {
+//   a = 10;
+//   var a;
+// }
+// console.log(a);
+// //block scope
+// {
+//   let b;
+//   b = 10;
+//   console.log(j);
+//   console.log(b);
+// }
+
+// const name = "sam";
+
+// name = "raj";
+
+// const students = {
+//   name: "raj",
+//   age: 40
+// };
+
+// students.name = "geeva";
+
+// console.log(students.name);
+
+// const classStudent = ["sam", "raj", "kumar"];
+
+// classStudent.push("ram");
+// console.log(classStudent);
